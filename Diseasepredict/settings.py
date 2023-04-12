d8878d2b547495b69ab32276cd97e1608b76c91c
Vikaskoppoju <98159254+Vikaskoppoju@users.noreply.github.com>
2023-04-12 11:36:45 +0530

    Update settings.py

--- Diseasepredict/settings.py
+++ Diseasepredict/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q+d^%i+2s=73vlmq)k#p1dvts6lasy5w85(-vc69jb$wek*$ax'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
